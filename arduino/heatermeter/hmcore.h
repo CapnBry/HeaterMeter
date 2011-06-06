e37a76ab61af887d415c70a0610a40936fca9c02
Bryan Mayland <bmayland@capnbry.net>
2011-06-06 18:23:40 -0400

    [hm] Add ATMega328 physical pin information to pins

--- arduino/heatermeter/hmcore.h
+++ arduino/heatermeter/hmcore.h
@@ -19,26 +19,27 @@ extern "C" {
 #include "hmmenus.h"
 
 // Analog Pins
-#define PIN_PIT     5
-#define PIN_FOOD1   4
-#define PIN_FOOD2   3
-#define PIN_AMB     2
-#define PIN_BUTTONS 0
+// Number in the comment is physical pin on ATMega328
+#define PIN_PIT     5       // 28
+#define PIN_FOOD1   4       // 27
+#define PIN_FOOD2   3       // 26
+#define PIN_AMB     2       // 25
+#define PIN_BUTTONS 0       // 24
 // Digital Output Pins
-#define PIN_SERIALRX     0  // Can not be changed
-#define PIN_SERIALTX     1  // Can not be changed
-#define PIN_WIFI_INT     2
-#define PIN_BLOWER       3
-#define PIN_LCD_CLK      4
-#define PIN_LCD_BACKLGHT 5
-#define PIN_ALARM        6
-#define PIN_SOFTRESET    7  // DataFlash SS on WiShield
-#define PIN_LCD_DATA     8
-#define PIN_WIRELESS_LED 9
-#define PIN_SPI_SS      10  // WiFi LED on WiShield
-#define PIN_SPI_MOSI    11  // Can not be changed
-#define PIN_SPI_MISO    12  // Can not be changed
-#define PIN_SPI_SCK     13  // Can not be changed
+#define PIN_SERIALRX     0  // 2 Can not be changed
+#define PIN_SERIALTX     1  // 3 Can not be changed
+#define PIN_WIFI_INT     2  // 4
+#define PIN_BLOWER       3  // 5
+#define PIN_LCD_CLK      4  // 6
+#define PIN_LCD_BACKLGHT 5  // 11
+#define PIN_ALARM        6  // 12
+#define PIN_SOFTRESET    7  // 13 DataFlash SS on WiShield
+#define PIN_LCD_DATA     8  // 14
+#define PIN_WIRELESS_LED 9  // 15
+#define PIN_SPI_SS      10  // 16
+#define PIN_SPI_MOSI    11  // 17 Can not be changed
+#define PIN_SPI_MISO    12  // 18 Can not be changed
+#define PIN_SPI_SCK     13  // 19 Can not be changed
 
 void hmcoreSetup(void);
 void hmcoreLoop(void);
