e1ba7e0a897695c8bbb68cb7c78a8801307ae95c
Bryan Mayland <bmayland@capnbry.net>
2011-07-05 12:16:14 -0400

    [lm] Added /admin/lm/stashdb
    Parameters:
    reset=1 means to just reset the current running database
    restore=1 restore from the stashpath to the active database
    rrd=(filename) the stash filename to stash to or from can not contain a path

--- openwrt/package/linkmeter/luasrc/controller/linkmeter/admin.lua
+++ openwrt/package/linkmeter/luasrc/controller/linkmeter/admin.lua
@@ -5,6 +5,8 @@ function index()
   entry({"admin", "lm", "home"}, alias("lm", "login"), "Home", 10)
   entry({"admin", "lm", "conf"}, template("linkmeter/conf"), "Configuration", 20)
   entry({"admin", "lm", "fw"}, call("action_fw"), "AVR Firmware", 30)
+
+  entry({"admin", "lm", "stashdb"}, call("action_stashdb"))
 end
 
 function action_fw()
@@ -40,5 +42,53 @@ function action_fw()
       "/usr/sbin/avrupdate %q" % hex)
     return luci.ltn12.pump.all(pipe, luci.http.write)
   end 
+end
+
+function action_stashdb()
+  local http = require "luci.http"
+  local uci = luci.model.uci.cursor()
+
+  local RRD_FILE = uci:get("linkmeter", "daemon", "rrd_file")
+  local STASH_PATH = uci:get("linkmeter", "daemon", "stashpath") or "/root"
+  local restoring = http.formvalue("restore")
+  local resetting = http.formvalue("reset")
+  local stashfile = http.formvalue("rrd") or "hm.rrd"
+
+  -- directory traversal
+  if stashfile:find("[/\\]+") then
+    http.status(400, "Bad Request")
+    http.prepare_content("text/plain")
+    return http.write("Invalid stashfile spefified: "..stashfile)
+  end
+
+  -- the stashfile should start with a slash
+  if stashfile:sub(1,1) ~= "/" then stashfile = "/"..stashfile end
+  -- and end with .rrd
+  if stashfile:sub(-4) ~= ".rrd" then stashfile = stashfile..".rrd" end
+
+  stashfile = STASH_PATH..stashfile
+
+  local result
+  http.prepare_content("text/plain")
+  if restoring == "1" or resetting == "1" then
+    luci.sys.call("/etc/init.d/linkmeterd stop")
+    if resetting == "1" then
+      result = nixio.fs.unlink(RRD_FILE)
+      http.write("Resetting "..RRD_FILE)
+    else
+      result = nixio.fs.copy(stashfile, RRD_FILE)
+      http.write("Restoring "..stashfile.." to "..RRD_FILE)
+    end
+    luci.sys.call("/etc/init.d/linkmeterd start")
+  else
+    result = nixio.fs.copy(RRD_FILE, stashfile)
+    http.write("Stashing "..RRD_FILE.." to "..stashfile)
+  end
+
+  if result then
+    http.write("\nOK")
+  else
+    http.write("\nERR")
+  end
 end
 
