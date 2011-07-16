fa53e72bda5e5901fc01f5d5193e7f100c04b05b
Bryan Mayland <bmayland@capnbry.net>
2011-07-16 14:39:45 -0400

    [lm] Add basic linkmeter socket client

---
+++ openwrt/package/linkmeter/luasrc/lmclient.lua
@@ -0,0 +1,25 @@
+require("nixio")
+require("nixio.fs")
+
+local client = nixio.socket("unix", "dgram")
+  -- autobind to an abstract address, optionally I could explicitly specify
+  -- and abstract name such as "\0linkmeter"..pid 
+  -- Abstract socket namespace is only supported on Linux
+client:bind("")
+if not client:connect("/var/run/linkmeter.sock") then
+  print("No connect")
+  client:close()
+  return false
+end
+
+local bytes = client:send(#arg > 0 and "$"..arg[1] or "$LMRF")
+print("Sent "..tostring(bytes))
+
+local polle = { fd = client, events = nixio.poll_flags("in"), revents = 0 }
+if nixio.poll({polle}, 1000) then
+  local inmsg = client:recv(1024)
+  print(inmsg)
+else
+  print("timeout")
+end
+client:close()
