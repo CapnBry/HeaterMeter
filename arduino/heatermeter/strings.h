7fa09db55c5b8427ff3095fdff5a0590558a8b81
Bryan Mayland <bmayland@capnbry.net>
2012-01-24 20:59:31 -0500

    [hm] UCID=201201240

--- arduino/heatermeter/strings.h
+++ arduino/heatermeter/strings.h
@@ -12,7 +12,7 @@
 
 #define DEGREE "\xdf" // \xdf is the degree symbol on the Hitachi HD44780
 const char LCD_LINE1_UNPLUGGED[] PROGMEM = "- No Pit Probe -";
-const char HM_VERSION[] PROGMEM = "201201180";
+const char HM_VERSION[] PROGMEM = "201201240";
 
 inline void Serial_char(char c) { Serial.write(c); }
 inline void Serial_nl(void) { Serial_char('\n'); }
