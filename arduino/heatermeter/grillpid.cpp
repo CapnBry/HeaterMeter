1a20872dad01733e296f8fd0921f2588a2cd26bf
Bryan Mayland <bmayland@capnbry.net>
2013-04-19 11:25:29 -0400

    [hm] Increase PID D lookback distance period from 39 sec to 60 sec
    The derivative is an exponential moving average so that is that it will
    reach 85% of its value the 45 second period. The Pid D constant should
    be reduced proportionally to achieve the same output.

--- arduino/heatermeter/grillpid.cpp
+++ arduino/heatermeter/grillpid.cpp
@@ -16,9 +16,9 @@ extern const GrillPid pid;
 #define TEMP_MEASURE_PERIOD 1000
 // The temperatures are averaged over 1, 2, 4 or 8 samples per period
 #define TEMP_AVG_COUNT 8
-// 1/(Number of samples used in the exponential moving average)
-#define TEMPPROBE_AVG_SMOOTH (1.0f/20.0f)
-#define FANSPEED_AVG_SMOOTH (1.0f/120.0f)
+// 2/(1+Number of samples used in the exponential moving average)
+#define TEMPPROBE_AVG_SMOOTH (2.0f/(1.0f+60.0f))
+#define FANSPEED_AVG_SMOOTH (2.0f/(1.0f+240.0f))
 // Once entering LID OPEN mode, the minimum number of seconds to stay in
 // LID OPEN mode before autoresuming due to temperature returning to setpoint 
 #define LIDOPEN_MIN_AUTORESUME 30
