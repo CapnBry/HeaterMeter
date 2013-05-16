1db5ee27c67d6025651314d44a9300044a33c5f9
Bryan Mayland <bmayland@capnbry.net>
2013-05-16 11:25:40 -0400

    [lm] Add 'light' index page. /luci/lm/light

--- openwrt/package/linkmeter/luasrc/controller/linkmeter/lm.lua
+++ openwrt/package/linkmeter/luasrc/controller/linkmeter/lm.lua
@@ -5,5 +5,18 @@ function index()
   root.target = alias("lm")
 
   entry({"lm"}, template("linkmeter/index"), nil, 10)
+  entry({"lm", "light"}, call("action_light_index"), nil, 20)
+end
+
+function action_light_index()
+  require "lmclient"
+  local json = require("luci.json")
+  local result, err = LmClient():query("$LMSU")
+  if result then
+    local lm = json.decode(result)
+    luci.template.render("linkmeter/light", {lm=lm, lmraw=result})
+  else
+    luci.dispatcher.error500("Stauts read failed: " .. err or "Unknown")
+  end
 end
 
