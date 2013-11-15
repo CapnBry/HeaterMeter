9c93770c0cd610f1d8d181e0410925cb048c126a
Bryan Mayland <bmayland@capnbry.net>
2013-11-15 10:53:23 -0500

    [lmr] Sync grillpid_conf

--- arduino/lmremote/grillpid_conf.h
+++ arduino/lmremote/grillpid_conf.h
@@ -15,6 +15,12 @@
 #define TEMP_MEASURE_PERIOD 1000
 // The temperatures are averaged over 1, 2, 4 or 8 samples per period
 #define TEMP_AVG_COUNT 1
+// 2/(1+Number of samples used in the exponential moving average)
+#define TEMPPROBE_AVG_SMOOTH (2.0f/(1.0f+60.0f))
+#define PIDOUTPUT_AVG_SMOOTH (2.0f/(1.0f+240.0f))
+// Once entering LID OPEN mode, the minimum number of seconds to stay in
+// LID OPEN mode before autoresuming due to temperature returning to setpoint
+#define LIDOPEN_MIN_AUTORESUME 30
 
 // Servo refresh period in usec, 20000 usec = 20ms = 50Hz
 #define SERVO_REFRESH          20000
