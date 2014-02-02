e2dbd26ff79b6a7e53f46041315b5f052eca79de
Bryan Mayland <bmayland@capnbry.net>
2014-02-02 10:17:07 -0500

    [wrt] msmtp allow account add/rename/delete

--- openwrt/package/luci-msmtp/luasrc/model/cbi/admin_services/msmtp.lua
+++ openwrt/package/luci-msmtp/luasrc/model/cbi/admin_services/msmtp.lua
@@ -4,6 +4,7 @@ require "nixio.util"
 local function OPT_SPLIT(o) return o:match("(%w+)\.(.+)") end
 
 local conf = {}
+local accountrenames = {}
 local f = nixio.open("/etc/msmtprc", "r")
 if f then
   local aname = "#"
@@ -26,26 +27,29 @@ if f then
   f:close()
 end
 
-if not conf.default then
-  local default = { account = "default" }
-  conf['default'] = default
-end
-
 -- Associated tables can't be sorted so make a copy in an indexed table
 -- so the accounts can be put in order
-local orderedconf = {}
-for _,v in pairs(conf) do
-  orderedconf[#orderedconf+1] = v
-end
-table.sort(orderedconf, function (a,b)
-  if a.account == "default" then
-    return true
-  elseif b.account == "default" then
-    return false
-  else
-    return a.account<b.account
+local orderedconf
+local function sortAccounts()
+  if not conf.default then
+    local default = { account = "default" }
+    conf['default'] = default
   end
-end)
+
+  orderedconf = {}
+  for _,v in pairs(conf) do
+    orderedconf[#orderedconf+1] = v
+  end
+  table.sort(orderedconf, function (a,b)
+    if a.account == "default" then
+      return true
+    elseif b.account == "default" then
+      return false
+    else
+      return a.account<b.account
+    end
+  end)
+end
 
 local m = SimpleForm("msmtp", "SMTP (msmtp) Email Client",
   [[This will rewrite the /etc/msmtprc file. Any comments in the file will
@@ -64,6 +68,7 @@ local m = SimpleForm("msmtp", "SMTP (msmtp) Email Client",
     if opt == "account" then
       -- restrict msmtp account name to only alphanumeric
       value = value:gsub("%W", "")
+      accountrenames[value] = acct
     end
     conf[acct][opt] = value
   end
@@ -73,82 +78,109 @@ function s.cfgvalue(self, section)
   return true
 end
 
-for _, acct in pairs(orderedconf) do
-  local aname = acct.account
-  s:tab(aname, aname)
-  local fld, dep
-  local depbase = s.config .. "." .. s.section .. "."
-  
-  fld = s:taboption(aname, Value, aname .. ".account", "Account name",
-    "Use 'default' if this is the only account")
-  fld.rmempty = false
-  
-  fld = s:taboption(aname, Value, aname .. ".host", "Server host name or IP")
-  fld.rmempty = false
-  fld.placeholder = "mail.domain.com"
-  fld.datatype = "host"
-  
-  fld = s:taboption(aname, Value, aname .. ".port", "Server port number")
-  fld.default = "25"
-  fld.datatype = "port"
-  
-  fld = s:taboption(aname, Value, aname .. ".from", "Email 'from' address")
-  fld.placeholder = "user@domain.com"
-  fld.optional = true
-  fld.datatype = "minlength(3)"
-  
-  fld = s:taboption(aname, Flag, aname .. ".auth", "Requires authentication")
-  fld.default = "off"
-  fld.disabled = "off"
-  fld.enabled = "on"
-  dep = {}
-  dep[depbase .. fld.option] = "on"
-  
-  fld = s:taboption(aname, Value, aname .. ".user", "Account user name")
-  fld.placeholder = "user"
-  fld:depends(dep)
-  
-  fld = s:taboption(aname, Value, aname .. ".password", "Account password")
-  fld.password = true
-  fld:depends(dep)
+local function buildAccountTabs()
+  s.tabs = {}
+  s.tab_names = {}
+  for _, acct in pairs(orderedconf) do
+    local aname = acct.account
+    s:tab(aname, aname)
+    local fld, dep
+    local depbase = s.config .. "." .. s.section .. "."
+
+    fld = s:taboption(aname, Value, aname .. ".account", "Account name",
+      "Use 'default' if this is the only account")
+    fld.optional = true
+    fld.rmempty = false
+
+    fld = s:taboption(aname, Value, aname .. ".host", "Server host name or IP")
+    fld.rmempty = false
+    fld.placeholder = "mail.domain.com"
+    fld.datatype = "host"
+
+    fld = s:taboption(aname, Value, aname .. ".port", "Server port number")
+    fld.default = "25"
+    fld.datatype = "port"
+
+    fld = s:taboption(aname, Value, aname .. ".from", "Email 'from' address")
+    fld.placeholder = "user@domain.com"
+    fld.optional = true
+    fld.datatype = "minlength(3)"
  
-  fld = s:taboption(aname, Flag, aname .. ".tls", "Enable TLS/SSL encryption")
-  fld.default = "off"
-  fld.disabled = "off"
-  fld.enabled = "on"
-  dep = {}
-  dep[depbase .. fld.option] = "on"
-  
-  fld = s:taboption(aname, Flag, aname .. ".tls_starttls", "Use STARTTLS",
-    "If connection is TLS this should be checked, if it is SSL leave unchecked")
-  fld.default = "on"
-  fld.disabled = "off"
-  fld.enabled = "on"
-  fld:depends(dep)
-  
-  fld = s:taboption(aname, Flag, aname .. ".tls_certcheck", "Verify server certificate",
-    "Only check if you've created a TLS trust file")
-  fld.default = "on"
-  fld.disabled = "off"
-  fld.enabled = "on"
-  fld:depends(dep)
-end -- for account
+    fld = s:taboption(aname, Flag, aname .. ".auth", "Requires authentication")
+    fld.default = 0
+    fld.disabled = "off"
+    fld.enabled = "on"
+    dep = {}
+    dep[depbase .. fld.option] = "on"
+
+    fld = s:taboption(aname, Value, aname .. ".user", "Account user name")
+    fld.placeholder = "user"
+    fld:depends(dep)
+
+    fld = s:taboption(aname, Value, aname .. ".password", "Account password")
+    fld.password = true
+    fld:depends(dep)
+
+    fld = s:taboption(aname, Flag, aname .. ".tls", "Enable TLS/SSL encryption")
+    fld.default = "off"
+    fld.disabled = "off"
+    fld.enabled = "on"
+    dep = {}
+    dep[depbase .. fld.option] = "on"
+
+    fld = s:taboption(aname, Flag, aname .. ".tls_starttls", "Use STARTTLS",
+      "If connection is TLS this should be checked, if it is SSL leave unchecked")
+    fld.default = "on"
+    fld.disabled = "off"
+    fld.enabled = "on"
+    fld:depends(dep)
+
+    fld = s:taboption(aname, Flag, aname .. ".tls_certcheck", "Verify server certificate",
+      "Only check if you've created a TLS trust file")
+    fld.default = "on"
+    fld.disabled = "off"
+    fld.enabled = "on"
+    fld:depends(dep)
+  end -- for account
+end -- buildAccountTabs()
+
+sortAccounts()
+buildAccountTabs()
 
 function m.handle(self, state, data)
   if state == FORM_VALID then 
     -- Convert the conf table into a list of strings 
     local newconf = {} 
-    for k,v in ipairs(orderedconf) do
-      -- Account has to be the first line of the config section
-      newconf[#newconf+1] = "account " .. v.account
-      for opt, val in pairs(v) do
-        if opt ~= "account" then
-          newconf[#newconf+1] = opt .. " " .. val
-        end
-      end -- for opt in account
-      newconf[#newconf+1] = ""
+    local reconf = {}
+    local formvalues = luci.http.formvalue()
+    local selected_tab = formvalues["tab.msmtp.Accounts"]
+
+    for _,v in pairs(conf) do
+      -- This prevents writing two accounts with the same name
+      v = conf[v.account] or v
+
+      if accountrenames[v.account] == selected_tab then
+        formvalues["tab.msmtp.Accounts"] = v.account or "default"
+      end
+
+      if v.account then
+        -- Account has to be the first line of the config section
+        newconf[#newconf+1] = "account " .. v.account
+        for opt, val in pairs(v) do
+          if opt ~= "account" then
+            newconf[#newconf+1] = opt .. " " .. val
+          end
+        end -- for opt in account
+        newconf[#newconf+1] = ""
+
+        reconf[v.account] = v
+     end -- if not blank account
     end -- for each account
-    
+
+    conf = reconf
+    sortAccounts()
+    buildAccountTabs()
+
     nixio.fs.writefile("/etc/msmtprc", table.concat(newconf, "\n"))
   end -- if form valid
 end
