9c4b36773155dfb21cc8a45ef8bfa692370b37d1
Bryan Mayland <bmayland@capnbry.net>
2014-03-19 10:16:35 -0400

    [avr] Rename econfig.cpp econfig.c

--- arduino/heatermeter/econfig.h
+++ arduino/heatermeter/econfig.h
@@ -9,11 +9,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void econfig_read_block(void *_dst, const void *_src, uint8_t n);
 uint16_t econfig_read_word(const void *_src);
 
 void econfig_write_block(const void *_src, void *_dst, uint8_t  n);
 void econfig_write_word(void *_dst, uint16_t val);
 void econfig_write_byte(void *_dst, uint8_t val);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif
