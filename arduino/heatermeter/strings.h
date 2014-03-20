5ce558f1fc831f90e0fa23b744b32bb944d03c48
Bryan Mayland <bmayland@capnbry.net>
2014-03-20 18:40:15 -0400

    [hm] Version 20140320 23780 bytes

--- arduino/heatermeter/strings.h
+++ arduino/heatermeter/strings.h
@@ -15,7 +15,7 @@
 #endif
 
 #define DEGREE "\xdf" // \xdf is the degree symbol on the Hitachi HD44780
-#define HM_VERSION "20131007"
+#define HM_VERSION "20140320"
 
 const char LCD_LINE1_UNPLUGGED[] PROGMEM = "- No Pit Probe -";
 
