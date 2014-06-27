a90440db8f5089385a3f05725137933243715f24
Bryan Mayland <bmayland@capnbry.net>
2014-06-27 11:23:48 -0400

    [wrt] Fix HTTP keepalive and enable for new linkmeter installs

--- openwrt/install.sh
+++ openwrt/install.sh
@@ -39,6 +39,7 @@ cp patches/217-luci-login-urltok.patch $LUCIP
 cp patches/218-lucid-cacheloader.patch $LUCIP
 cp patches/219-lucid-sysupgrade-url.patch $LUCIP
 cp patches/226-lucid-no-redirect.patch $LUCIP
+cp patches/227-lucid-keepalive-fix.patch $LUCIP
 
 LMPACKS="rrdtool kmod-broadcom-sdhc26 linkmeter kmod-8192cu kmod-spi-bcm2708 luci-app-msmtp parted avahi-daemon kmod-incompat-wireless"
 for PACK in $LMPACKS ; do
