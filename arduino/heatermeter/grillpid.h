406531e4d2d57981494d59c218d5287c8263eda7
R-Burk <robertcburk@gmail.com>
2014-10-12 19:38:07 -0400

    Revert "[hm] Output fan speed percentage in LMSU, needs linkmeterd update"
    This reverts commit dafb2ff3a31101d1f50edb67cbf8491c4ea538d3.

--- arduino/heatermeter/grillpid.h
+++ arduino/heatermeter/grillpid.h
@@ -157,15 +157,11 @@ private:
   unsigned char _minFanSpeed;
   unsigned char _maxServoPos;
   unsigned char _minServoPos;
+  unsigned char _lastBlowerOutput;
   unsigned char _outputFlags;
 
-  // Current fan speed (percent)
-  unsigned char _fanSpeed;
   // Feedback switching mode voltage controller
   unsigned char _feedvoltLastOutput;
-  // Desired fan target (0-255)
-  unsigned char _lastBlowerOutput;
-  // Target servo position (ticks)
   int _servoTarget;
   int _servoStep;
   
@@ -227,7 +223,7 @@ public:
   unsigned char getPidOutput() const { return _pidOutput; }
   void setPidOutput(int value);
   // Current fan speed output in percent
-  unsigned char getFanSpeed(void) const { return _fanSpeed; };
+  unsigned char getFanSpeed(void) const;
   unsigned long getLastWorkMillis(void) const { return _lastWorkMillis; }
 
   boolean getManualOutputMode(void) const { return _manualOutputMode; }
