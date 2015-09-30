789e19adb80193f2f7c08c193124f9799413d09b
Bryan Mayland <bmayland@capnbry.net>
2015-09-30 10:36:33 -0400

    [wrt] Fixup luci feed definition and add hostname support to DHCP client mode

--- openwrt/install.sh
+++ openwrt/install.sh
@@ -23,21 +23,21 @@ fi
 if [ "$TARGET" = "BCM2708" ] ; then
   cat << EOFEEDS > $WRT_PATH/feeds.conf
 src-svn packages svn://svn.openwrt.org/openwrt/branches/packages_12.09
-src-svn luci http://svn.luci.subsignal.org/luci/tags/0.11.1/contrib/package
+src-git luci http://github.com/openwrt/luci.git;luci-0.11
 src-link linkmeter $REPO_PATH/package
 EOFEEDS
 fi
 
 $WRT_PATH/scripts/feeds update
 
-LUCIP=$WRT_PATH/feeds/luci/luci/patches
+LUCIP=$WRT_PATH/feeds/luci/patches
 rm -fR $LUCIP
 mkdir $LUCIP
 cp patches/200-luci-inreq-fix.patch $LUCIP
 cp patches/215-luci-adminfull-inreq.patch $LUCIP
 cp patches/217-luci-login-urltok.patch $LUCIP
 cp patches/218-lucid-cacheloader.patch $LUCIP
-cp patches/219-lucid-sysupgrade-url.patch $LUCIP
+cp patches/219-luci-sysupgrade-url.patch $LUCIP
 cp patches/226-lucid-no-redirect.patch $LUCIP
 cp patches/227-lucid-keepalive-fix.patch $LUCIP
 cp patches/228-luci-no-dhcp-dhcpc.patch $LUCIP
@@ -58,6 +58,7 @@ if [ "$TARGET" = "BCM2708" ] ; then
   patch -N -p0 -d $WRT_PATH < patches/110-dnsmasq_add_hostname.patch
   patch -N -p0 -d $WRT_PATH < patches/220-iwinfo-nl80211-over-wext.patch
   patch -N -p0 -d $WRT_PATH < patches/225-iwinfo-scan-wo-vintf.patch
+  patch -N -p0 -d $WRT_PATH < patches/229-netifd-add-hostname.patch
   cp patches/06??-rpi-patches-*.patch \
     $WRT_PATH/target/linux/brcm2708/patches-3.3/.
 fi
