fa896413e53de8e0207e4a11c69d6a991d44f066
Steve Mickeler <steve@warning.ca>
2015-10-04 02:49:21 -0400

    add 230-hotplug2-source.patch file and enable in install.sh

--- openwrt/install.sh
+++ openwrt/install.sh
@@ -60,6 +60,7 @@ if [ "$TARGET" = "BCM2708" ] ; then
   patch -N -p0 -d $WRT_PATH < patches/220-iwinfo-nl80211-over-wext.patch
   patch -N -p0 -d $WRT_PATH < patches/225-iwinfo-scan-wo-vintf.patch
   patch -N -p0 -d $WRT_PATH < patches/229-netifd-add-hostname.patch
+  patch -N -p0 -d $WRT_PATH < patches/230-hotplug2-source.patch
   cp patches/06??-rpi-patches-*.patch \
     $WRT_PATH/target/linux/brcm2708/patches-3.3/.
 fi
