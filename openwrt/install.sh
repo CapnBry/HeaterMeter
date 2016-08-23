b8698ebea4a2e2f4f1b514c44af75607c3753c17
Bryan Mayland <bmayland@capnbry.net>
2016-08-23 10:48:53 -0400

    [wrt] Remove clearsilver fix, openssl heartbleed, brcm2708-firmware, hotplug patches
    -- clearsilver is no longer used by wrt
    -- openssl package is up to date with heartbleed fix
    -- brcm2708-firmware upstream now has my changes
    -- hotplug is current in trunk

--- openwrt/install.sh
+++ openwrt/install.sh
@@ -48,12 +48,9 @@ fi
 
 if [ "$TARGET" = "BCM2708" ] ; then
   patch -N -p0 -d $WRT_PATH < patches/0700-bcm2708-tweaks.patch
-  patch -N -p0 -d $WRT_PATH < patches/0710-brcm2708-firmware.patch
   patch -N -p0 -d $WRT_PATH < patches/110-dnsmasq_add_hostname.patch
   patch -N -p0 -d $WRT_PATH < patches/220-iwinfo-nl80211-over-wext.patch
   patch -N -p0 -d $WRT_PATH < patches/225-iwinfo-scan-wo-vintf.patch
   patch -N -p0 -d $WRT_PATH < patches/229-netifd-add-hostname.patch
-  patch -N -p0 -d $WRT_PATH < patches/230-hotplug2-source.patch
-  patch -N -p0 -d $WRT_PATH < patches/231-clearsilver-utf8.patch
 fi
 
