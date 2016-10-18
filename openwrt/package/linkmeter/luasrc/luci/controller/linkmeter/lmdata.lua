121bf8df5ce5351ffb453ad796686fd83e43b0da
Bryan Mayland <bmayland@capnbry.net>
2016-10-18 18:23:27 -0400

    [lm] Add the ability to save PNG images of live graphs as well as download CSV as attachment
    Adds a /lm/dluri controller which takes a data URI as a parameter and spits it back as
    content-disposition attachment which prompts the user to save as. Also removes the PNG toggle
    on the archive page in leiu of using this new portable download method.
    
    This changes the graph to always use a canvas, not sure if that will have any side effects.

--- openwrt/package/linkmeter/luasrc/luci/controller/linkmeter/lmdata.lua
+++ openwrt/package/linkmeter/luasrc/luci/controller/linkmeter/lmdata.lua
@@ -6,11 +6,12 @@ function index()
   entry({"lm", "rfstatus"}, call("action_rfstatus")).notemplate = true
   entry({"lm", "stream"}, call("action_stream")).notemplate = true
   entry({"lm", "conf"}, call("action_conf")).notemplate = true
+  entry({"lm", "dluri"}, call("action_downloaduri")).notemplate = true
 end
 
 function lmclient_json(query, default)
   require "lmclient"
-  local result, err = LmClient():query(query) 
+  local result, err = LmClient():query(query)
   result = result or default
   if result then
     luci.http.prepare_content("application/json")
@@ -42,16 +43,16 @@ function action_hist()
   local RRD_FILE = http.formvalue("rrd") or uci:get("linkmeter", "daemon", "rrd_file")
   local nancnt = tonumber(http.formvalue("nancnt"))
   local start, step, data, soff
-  
+
   if not nixio.fs.access(RRD_FILE) then
-    http.status(503, "Database Unavailable")  
+    http.status(503, "Database Unavailable")
     http.prepare_content("text/plain")
     http.write("No database: %q" % RRD_FILE)
     return
   end
-  
+
   local now = rrd.last(RRD_FILE)
-  
+
   if not nancnt then
     -- scroll through the data and find the first line that has data
     -- this should indicate the start of data recording on the largest
@@ -67,7 +68,7 @@ function action_hist()
       nancnt = nancnt + 1
     end
   end
-    
+
   if nancnt >= 460 then
     step = 10
     soff = 3600
@@ -83,18 +84,24 @@ function action_hist()
   end
 
   -- Make sure our end time falls on an exact previous or now time boundary
-  now = math.floor(now/step) * step  
+  now = math.floor(now/step) * step
 
-  -- Only pull new data if the nancnt probe data isn't what we're looking for 
+  -- Only pull new data if the nancnt probe data isn't what we're looking for
   if step ~= 180 or not data then
     start, step, _, data = rrd.fetch(RRD_FILE, "AVERAGE",
       "--end", now, "--start", now - soff, "-r", step)
   end
-  
-  http.prepare_content("text/plain")
+
+  if http.formvalue("dl") == "1" then
+    local csvfile = nixio.fs.basename(RRD_FILE:sub(1, -5)) .. ".csv"
+    http.prepare_content("text/csv")
+    http.header("Content-Disposition", 'attachment; filename="' .. csvfile ..'"')
+  else
+    http.prepare_content("text/plain")
+  end
   http.header("Cache-Control", "max-age="..step)
 
-  local seenData 
+  local seenData
   now = now - step
   for _, dp in ipairs(data) do
     -- Skip the first NaN rows until we actually have data and keep
@@ -103,9 +110,9 @@ function action_hist()
       http.write(("%u,%s\n"):format(start, table.concat(dp, ",")))
       seenData = true
     end
-    
+
     start = start + step
-  end 
+  end
 end
 
 function action_stream()
@@ -116,4 +123,56 @@ function action_stream()
     http.write(o)
     collectgarbage("collect")
   end)
+end
+
+function action_downloaduri()
+  -- Takes a data URI input and converts it to a file download
+  -- It is dumb that most browsers do not support saving a local data URI to file
+  -- but this works around it by having the client generate the image and the server
+  -- just sends it back to them.
+  -- LuCI only supports POST data larger than 8KB in a multipart/form-data, be
+  -- sure to use a form with encoding set to this.
+  -- Field "uri" the data URI (including data: and metadata)
+  -- Field "filename" (optional) filename to set as the attachment name
+  local nixio = require "nixio"
+  local http = require "luci.http"
+  local mime = require "luci.http.protocol.mime"
+  local uri = http.formvalue("uri")
+  if (not uri or uri:sub(1, 5) ~= "data:") then
+    http.status(400, "URI must begin with 'data:'")
+    return
+  end
+
+  local comma = uri:find(',')
+  if (not comma) then
+    http.status(400, "URI missing metadata")
+    return
+  end
+
+  local metadata = uri:sub(6, comma-1)
+  local mimetype = "text/plain"
+  local encoding = ""
+  local charset = "US-ASCII"
+  local matchcnt = 0
+  for meta in metadata:gmatch("[^;]+") do
+    if matchcnt == 0 then
+      mimetype = meta
+    elseif meta:sub(1,8) == "charset=" then
+      charset = meta:sub(9)
+    else
+      encoding = meta
+    end
+    matchcnt = matchcnt + 1
+  end
+
+  local filename = http.formvalue("filename") or
+    (os.date("%Y%m%d_%H%M%S.") .. (mime.to_ext(mimetype) or "txt"))
+  http.prepare_content(mimetype)
+  http.header('Content-Disposition', 'attachment; filename="' .. filename .. '"')
+  if encoding == "base64" then
+    uri = nixio.bin.b64decode(uri:sub(comma+1))
+  else
+    uri = uri:sub(comma+1)
+  end
+  http.write(uri)
 end
