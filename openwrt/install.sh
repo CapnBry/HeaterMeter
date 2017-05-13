593c3f174619761547eaa8ce5b9c242bf5c2e01f
Bryan Mayland <bmayland@capnbry.net>
2017-05-13 14:38:14 -0400

    [wrt] Update to latest LEDE (Linux 4.9.20)

--- openwrt/install.sh
+++ openwrt/install.sh
@@ -8,7 +8,7 @@ if [ -z "$WRT_PATH" ] ; then
   echo "Usage: ./install.sh <target> <wrt path>"
   echo "    Target: BCM47XX or BCM2708 (case sensitive)"
   echo "BCM47XX - svn://svn.openwrt.org/openwrt/trunk@29665"
-  echo "BCM2708 - https://github.com/lede-project/source.git@d17cb4a68a4544dca690629ee9a2483da29fa42a"
+  echo "BCM2708 - https://github.com/lede-project/source.git@1a16cb9c67f0d2c530914aec31c721b75f03a908"
   echo ""
   echo "Be sure both your WRT_PATH and WRT_PATH/feeds/luci are 'git reset --hard HEAD'"
   exit 1
