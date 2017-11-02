557871585a9bc13bf4363fc5dd044c6f1a832004
Bryan Mayland <bmayland@capnbry.net>
2017-11-02 09:17:47 -0400

    [wrt] Update to LEDE head fbde9ac7

--- openwrt/install.sh
+++ openwrt/install.sh
@@ -8,7 +8,7 @@ if [ -z "$WRT_PATH" ] ; then
   echo "Usage: ./install.sh <target> <wrt path>"
   echo "    Target: BCM47XX or BCM2708 (case sensitive)"
   echo "BCM47XX - svn://svn.openwrt.org/openwrt/trunk@29665"
-  echo "BCM2708 - https://github.com/lede-project/source.git@438dcbfe74a6da1a8949d8fb785092d934499b60"
+  echo "BCM2708 - https://github.com/lede-project/source.git@fbde9ac718409720a937671f3354837223b5db76"
   echo ""
   echo "Be sure both your WRT_PATH and WRT_PATH/feeds/luci are 'git reset --hard HEAD'"
   exit 1
@@ -24,10 +24,10 @@ fi
 
 if [ "$TARGET" = "BCM2708" ] ; then
   cat << EOFEEDS > $WRT_PATH/feeds.conf
-src-git packages https://git.lede-project.org/feed/packages.git^f9e99848182fc7bc554e541ca133c22079d4041b
-src-git luci https://git.lede-project.org/project/luci.git^29fabe26399fbaecf9231e24f9ac1ee5773cafa6
-#src-git routing https://git.lede-project.org/feed/routing.git^04a37ef4309c2b67c64901eb8fbf3800b4c7bb35
-#src-git telephony https://git.lede-project.org/feed/telephony.git^1f0fb2538ba6fc306198fe2a9a4b976d63adb304
+src-git packages https://git.lede-project.org/feed/packages.git^9f6eee3960125e75b9d09e4ea80dbf7a9de2b455
+src-git luci https://git.lede-project.org/project/luci.git^5e2800232efad8c4edd3d284b2a048319fe83bd8
+#src-git routing https://git.lede-project.org/feed/routing.git
+#src-git telephony https://git.lede-project.org/feed/telephony.git
 src-link linkmeter $REPO_PATH/package
 EOFEEDS
 fi
@@ -41,7 +41,7 @@ done
 
 # BRY: Want luci-ssl-openssl, but that adds luci -> luci-app-firewall -> iptables -> kmods-*
 # Think we can get away with linkmeter -> luci-mod-admin-full and then just enabling uhttpd / openssl-util
-LMPACKS="parted rrdtool linkmeter liblmfit-lua luci-theme-material luci-theme-openwrt luci-app-msmtp"
+LMPACKS="parted rrdtool linkmeter luci-mod-admin-full liblmfit-lua luci-theme-material luci-theme-openwrt luci-app-msmtp"
 for PACK in $LMPACKS ; do
   $WRT_PATH/scripts/feeds install -p linkmeter $PACK
 done
@@ -58,8 +58,7 @@ fi
 
 if [ "$TARGET" = "BCM2708" ] ; then
   cp diffconfig.$TARGET $WRT_PATH/.config
-  patch -N -p0 -d $WRT_PATH < patches/0700-bcm2708-tweaks.patch
-  patch -N -p0 -d $WRT_PATH < patches/229-netifd-add-hostname.patch
-  patch -N -p0 -d $WRT_PATH < patches/900-dnsmasq-2-78.patch
+  patch -N -p1 -d $WRT_PATH < patches/0700-bcm2708-tweaks.patch
+  patch -N -p1 -d $WRT_PATH < patches/229-netifd-add-hostname.patch
 fi
 
